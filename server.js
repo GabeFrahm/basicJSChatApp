0e3aee82b550fa3ba87b51d950a2ed89ccd590a9
Gabe Frahm <gabefrahm@gmail.com>
2020-04-23 21:09:56 -0400

    added recieving past messages

--- server.js
+++ server.js
@@ -6,26 +6,30 @@ let io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
 // Listen to the port
 http.listen(3000, () => {
   console.log('Listening on 3000');
 });
 
+let msgs = [];
 // Events
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('User connected');
+
+  // Sending past messages
+  console.log(socket.id);
+  io.to(socket.id).emit('past messages', msgs);
 
   // On Message
   socket.on('chat message', (usr, msg) => {
+    msgs.push([usr, msg]);
+    //console.log(msgs);
     io.emit('chat message', usr, msg);
   });
 
   // On Disconnect
   socket.on('disconnect', () => {
     console.log('User disconnected');
+    //TODO: Send a message along the lines of '{usr} disconnected'
   });
 });
